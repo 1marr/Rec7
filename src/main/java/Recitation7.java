49c87663a1e9f7e4ee97370bcd47ffc5568b1459
Mardinae Gary <mjgary21@students.desu.edu>
2022-12-07 16:05:15 -0500

    completed

--- src/main/java/Recitation7.java
+++ src/main/java/Recitation7.java
@@ -10,25 +10,39 @@ public class Recitation7
 {
     // define your constants after this comment
 
+    private static String generateCharacter(char ch, int num) {
+        String str = "";
+        for (int count = 0; count < num; count++) {
+            str += ch;
+        }
+        return str;
+    }
 
     public static String leftRightTriangle(int height)
     {
         String str = "";
-        //add your code here
+        for(int row=1; row <= height; row++){
+            str += generateCharacter('*', row)+"\n";
+        }
         return str;
     }
 
     public static String rightRightTriangle(int height)
     {
         String str = "";
-        //add your code here
+        for(int row = 1; row <= height; row++){
+            str += generateCharacter( ' ', height - row) + generateCharacter('*', row)+"\n";
+        }
         return str;
     }
 
     public static String circle(int radius)
     {
         String str = "";
-        //add your code here
+        for(int y = radius-1; y > -radius; y--){
+            int x = (int)(Math.sqrt((radius*radius) - (y*y)));
+            str += generateCharacter(' ', radius-x) + generateCharacter('*',2*x)+"\n";
+        }
         return str;
     }
 }
